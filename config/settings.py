b674f2627ab01295c7c1af2a83a781a7a902bdba
Eben Lal <ebenlal58@gmail.com>
2025-11-05 23:14:27 +0530

    otp mail

--- config/settings.py
+++ config/settings.py
@@ -183,6 +183,11 @@ MEDIA_ROOT = BASE_DIR/'media'
 
 AUTH_USER_MODEL = "accounts.User"
 
-# Use console backend for testing
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-DEFAULT_FROM_EMAIL = "no-reply@example.com"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'zecserbusiness@gmail.com'
+EMAIL_HOST_PASSWORD = 'wlsx ausq sxkm qxhr'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
